d785d01aaffa666507a91396c11ebdd6bf140bdb
Peter H. Boling <peter.boling@gmail.com>
2026-07-26 02:32:10 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/sanitize_email/version.rb
+++ lib/sanitize_email/version.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 module SanitizeEmail
+  # Version namespace for this gem.
   module Version
+    # Current gem version.
     VERSION = "2.0.14"
   end
+  # Current gem version exposed at the traditional constant location.
   VERSION = Version::VERSION # Traditional Constant Location
 end

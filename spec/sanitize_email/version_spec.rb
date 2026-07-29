bb2ab53398ddf6b0fbba5100be4552bdedc1889a
Peter H. Boling <peter.boling@gmail.com>
2026-07-29 00:51:31 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/sanitize_email/version_spec.rb
+++ spec/sanitize_email/version_spec.rb
@@ -3,8 +3,11 @@ RSpec.describe SanitizeEmail::Version do
   it_behaves_like "a Version module", described_class
 
   it "executes the version file for coverage without redefining constants" do
-    path = File.expand_path("../../lib/sanitize_email/version.rb", __dir__)
-    anonymous_namespace = AnonymousLoader.load(files: path)
+    paths = [
+      File.expand_path("../../lib/sanitize_email/version.rb", __dir__),
+      File.expand_path("../../lib/sanitize_email/version_gem.rb", __dir__)
+    ].select { |path| File.file?(path) }
+    anonymous_namespace = AnonymousLoader.load(files: paths)
 
     expect(anonymous_namespace::SanitizeEmail::Version::VERSION).to eq(described_class::VERSION)
   end

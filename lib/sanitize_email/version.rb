d58feb0efa591ce66d5db3485115fce832cbe3d0
Peter H. Boling <peter.boling@gmail.com>
2026-08-08 21:07:17 -0600

    🔖 Bump gem version

--- lib/sanitize_email/version.rb
+++ lib/sanitize_email/version.rb
@@ -4,7 +4,7 @@ module SanitizeEmail
   # Version namespace for this gem.
   module Version
     # Current gem version.
-    VERSION = "2.0.14"
+    VERSION = "2.0.15"
   end
   # Current gem version exposed at the traditional constant location.
   VERSION = Version::VERSION # Traditional Constant Location
